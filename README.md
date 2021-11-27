d165d5c44bdade2d411fdaf98b99110a18ab1659
XNM1 <artemx8zx@gmail.com>
2021-11-27 14:38:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# facebook-auth-node
+
+## Table of Contents
+
+- [About](#about)
+- [Usage](#usage)
+
+## About
+
+Simple facebook auth example on server side node.js
+
+### Installing
+
+```
+pnpm i #or npm install
+```
+
+## Usage
+
+```
+pnpm start #or npm start
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const hbs = require('express-hbs')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const FacebookStrategy = require('passport-facebook').Strategy
+
+passport.use(new FacebookStrategy({
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: `https://${process.env.HOST}:${process.env.PORT}${process.env.FACEBOOK_CALLBACK_URL}`
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        return cb(null, profile)
+    }
+))
+
+const authorizationMiddleware = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next()
+    } else {
+        res.redirect("/login")
+    }
+}
+const facebookAuthentication = passport.authenticate('facebook')
+const facebookAuthenticationCallback = passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}, (req, res) => {
+    res.redirect('/')
+})
+
+app.use(cookieSession({
+    maxAge: 90 * 24 * 60 * 60 * 1000,
+    keys: [process.env.SESSION_KEY]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.engine('handlebars', hbs.express4())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.get('/', authorizationMiddleware, (req, res) => {
+    res.render('home', {
+        id: req.user.id,
+        name: req.user.displayName
+    })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/auth/facebook', facebookAuthentication)
+
+app.get('/auth/facebook/callback', facebookAuthenticationCallback)
+
+app.get('/logout', authorizationMiddleware, (req, res) => {
+    req.logout()
+})
+
+app.listen(process.env.PORT, function () {
+    console.log(`Server listens https://${process.env.HOST}:${process.env.PORT}`)
+})
